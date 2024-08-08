9e054536d44ca9be5bdfc084a6ec36503274b599
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-08 10:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	   // Get the values of the first name and last name fields
+            let firstName = document.getElementById('fname').value;
+            let lastName = document.getElementById('lname').value;
+            
+            // Create the alert message
+            let message = firstName + " " + lastName;
+            
+            // Display the alert
+            alert(message);
 
 }
